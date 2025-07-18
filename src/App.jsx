f1b4da9f1e9821be53e512845e6b2b213fce5827
FrancescoCellitti <francescocellitti@virgilio.it>
2025-07-18 13:06:19 +0200

    added base card

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,56 @@
 import { useState } from 'react'
 
 function App() {
-  const initial =[
+  const initial = [
     'ciambellone',
     'torta'
   ]
   const [newTask, setNewTask] = useState('')
-  const [tasks, newTasks]= useState(initial)
-  function handleSubmit(e){
+  const [tasks, newTasks] = useState(initial)
+  function handleSubmit(e) {
     e.preventDefault()
     newTasks([newTask, ...tasks])
   }
   return (
     <>
-        <div className="container">
+      <div className="container">
 
-          <form onSubmit={handleSubmit}>
-            <div className="d-flex">
-              <input className='form-control mt-2' placeholder='insert here' type='text'  onChange={event => setNewTask(event.target.value)} />
-              <button type='button' className='btn btn-primary mx-2'>save</button>
-            </div>
-          </form>
-
-          <ul className='list-group mt-5'>
-            {tasks.map((task, index)=>{
+        <form onSubmit={handleSubmit}>
+          <div className="d-flex">
+            <input className="'form-control mt-2'" placeholder="'insert here'" type="text" onChange={event => setNewTask(event.target.value)} />
+            <button type='button' className='btn btn-primary mx-2'>save</button>
+          </div>
+        </form>
+        <div  className="container">
+          <div className="row mt-5">
+            {tasks.map((task, index) => {
               return (
-                <li key={index} className="list-group-item d-flex justify-content-between">{task}</li>
+                <div key={index} className="col-3">
+                  <div className="card">
+                    <div className="card-title">
+                      {task}
+                    </div>
+                  </div>
+                </div>
               )
             })}
-          </ul>
+          </div>
         </div>
-     
+
+       {/*  {tasks.map((task, index) => {
+          return (
+            <div className="col-3">
+              <div className="card">
+                <div className="card-title">
+                  {tasks}
+                </div>
+              </div>
+            </div>
+          )
+        })}  */}
+
+      </div>
+
     </>
   )
 }
