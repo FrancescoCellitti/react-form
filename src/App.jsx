a6792afb98643791c50795ff2f15e41c2ea44095
FrancescoCellitti <francescocellitti@virgilio.it>
2025-07-18 12:43:09 +0200

    added logic for basic todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const initial =[
+    'ciambellone',
+    'torta'
+  ]
+  const [newTask, setNewTask] = useState('')
+  const [tasks, newTasks]= useState(initial)
+  function handleSubmit(e){
+    e.preventDefault()
+    newTasks([newTask, ...tasks])
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <div className="container">
+
+          <form onSubmit={handleSubmit}>
+            <div className="d-flex">
+              <input className='form-control mt-2' placeholder='insert here' type='text'  onChange={event => setNewTask(event.target.value)} />
+              <button type='button' className='btn btn-primary mx-2'>save</button>
+            </div>
+          </form>
+
+          <ul className='list-group mt-5'>
+            {tasks.map((task, index)=>{
+              return (
+                <li key={index} className="list-group-item d-flex justify-content-between">{task}</li>
+              )
+            })}
+          </ul>
+        </div>
+     
     </>
   )
 }
